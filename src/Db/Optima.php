948d89c485df3b08aca0d5153ca704b4389f79c6
devunplugged <66313338+devunplugged@users.noreply.github.com>
2022-07-06 08:56:36 +0200

    db connection singleton added

---
+++ src/Db/Optima.php
@@ -0,0 +1,27 @@
+<?php
+namespace DeltaTools\Db;
+
+require_once $base_dir . 'db/db_mssql.inc';
+
+class Optima
+{
+    public static $instance = null;
+    private $db;
+
+    private function __construct()
+    {
+        if(!$this->db){
+            $this->db = new ps_mssqlDB();
+            $this->db->connect();
+        }
+    }
+
+    public static function connect()
+    {
+        if(!self::$instance){
+            self::$instance = new self();
+        }
+        return self::$instance->db;
+    }
+
+}
