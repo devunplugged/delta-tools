0fafa700120f7ed0ab5a027ae62976ce345dd0c9
devunplugged <66313338+devunplugged@users.noreply.github.com>
2022-09-24 12:54:18 +0200

    json success response codes added

--- src/Utilities/Response/Json.php
+++ src/Utilities/Response/Json.php
@@ -4,23 +4,27 @@ namespace DeltaTools\Utilities\Response;
 
 class Json
 {
-    public static function success(array $data, int $code = 200)
+    public static function success(int $code = 200, array $data = [], string $message = 'success')
     {
         $code = ($code < 200 || $code > 299) ? 200 : $code;
         http_response_code($code);
         header('Content-Type: application/json; charset=utf-8');
+
+        $data['message'] = $message;
+        $data['response-code'] = $code;
+
         echo json_encode($data);
         exit;
     }
 
-    public static function fail(string $message, int $code, array $errors = [], int $customCode = 0)
+    public static function fail(int $code, string $message = 'error', array $errors = [], int $customCode = 0)
     {
         http_response_code($code);
         header('Content-Type: application/json; charset=utf-8');
 
         $response = [
             'message' => $message, 
-            'code' => $code,
+            'response-code' => $code,
         ];
 
         if(!empty($errors)){
