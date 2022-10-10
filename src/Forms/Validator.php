fb35e542d4bf791e08f339e6d4a377fea308734a
devunplugged <66313338+devunplugged@users.noreply.github.com>
2022-10-10 13:07:37 +0200

    validation class update

--- src/Forms/Validator.php
+++ src/Forms/Validator.php
@@ -6,6 +6,109 @@ class Validator
 {
     private $generalErrors = [];
     private $validationErrors = [];
+    private $allowedValidations = ['required', 'isNumber', 'numberRange'];
+   
+    public function once($once)
+    {
+        if (!\DeltaTools\Utilities\Forms\Once::check($once)) {
+            $this->generalErrors[] = 'Ten formularz został już zapisany.';
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Uruchamia wiele walidacji podanej wartosci
+     */
+    public function validate($key, $values, $validations = '', $validationArgs = [], $customMessages = [], $isGeneral = false )
+    {
+        $validations = explode('|', $validations);
+
+        foreach($validations as $validation){
+            if(!in_array($validation, $this->allowedValidations) || !method_exists($this, $validation)){
+                continue;
+            }
+
+            if($validationArgs[$validation]){
+                $this->$validation(
+                    $key, 
+                    $values, 
+                    $customMessages[$validations] ?? '',
+                    $isGeneral,
+                    ...$validationArgs[$validation]
+                );
+            }else{
+                $this->$validation(
+                    $key, 
+                    $values, 
+                    $customMessages[$validations] ?? '',
+                    $isGeneral
+                );
+            }
+        }
+    }
+
+    /**
+     * Sprawdza czy podana wartosc jest liczba (is_numeric)
+     */
+    public function required($key, $values, $customMessage = '', $isGeneral = false)
+    {
+        if (!isset($values[$key])) {
+
+            if(!$isGeneral){
+                $this->addValidationError($key, $customMessage ?? 'Ta wartość jest wymagana');
+            }else{
+                $this->generalErrors[] = $customMessage ?? $key . ': Ta wartość jest wymagana';
+            }
+            
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Sprawdza czy podana wartosc jest liczba (is_numeric)
+     */
+    public function isNumber($key, $values, $customMessage = '', $isGeneral = false)
+    {
+        if (!is_numeric($values[$key])) {
+
+            if(!$isGeneral){
+                $this->addValidationError($key, $customMessage ?? 'Ta wartość powinna być liczbą');
+            }else{
+                $this->generalErrors[] = $customMessage ?? $key . ': Ta wartość powinna być liczbą';
+            }
+            
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Sprawdza czy liczba miesci sie w przedziale
+     */
+    public function numberRange($key, $values, $customMessage = '', $isGeneral = false, $from = 'INF', $to = 'INF')
+    {
+        if (!$this->isNumber($key, $values[$key])) {
+            return false;
+        }
+
+        if (
+            ($from != 'INF' && $to == 'INF' && $values[$key] < $from) ||
+            ($from == 'INF' && $to != 'INF' && $values[$key] > $to) ||
+            ($from != 'INF' && $to != 'INF' && ($values[$key] > $to || $values[$key] < $from))
+        ) {
+
+            if(!$isGeneral){
+                $this->addValidationError($key, $customMessage ?? 'Ta wartość powinna mieścić się w przedziale od ' . $from . ' do ' . $to);
+            }else{
+                $this->generalErrors[] = $customMessage ?? $key . ': Ta wartość powinna mieścić się w przedziale od ' . $from . ' do ' . $to;
+            }
+            return false;
+        }
+
+        return true;
+    }
 
     public function getGeneralErrors()
     {
@@ -19,11 +122,6 @@ class Validator
             $list .= '<li>' . $generalError . '</li>';
         }
         return $list . '</ul>';
-    }
-
-    public function hasGeneralErrors()
-    {
-        return !empty($this->generalErrors);
     }
 
     public function getValidationErrors()
@@ -42,48 +140,12 @@ class Validator
         }
         return $list . '</ul>';
     }
-    
-    public function once($once)
-    {
-        if (!\DeltaTools\Utilities\Forms\Once::check($once)) {
-            $this->generalErrors[] = 'Ten formularz został już zapisany.';
-            return false;
-        }
-        return true;
-    }
-
-    public function isNumber($key, $value)
-    {
-        if (!is_numeric($value)) {
-            $this->addValidationError($key, 'Ta wartość powinna być liczbą');
-            return false;
-        }
-        return true;
-    }
 
     public function isValid()
     {
         if(!empty($this->generalErrors) || !empty($this->validationErrors)){
             return false;
         }
-        return true;
-    }
-
-    public function numberRange($key, $value, $from = 'INF', $to = 'INF')
-    {
-        if (!$this->isNumber($key, $value)) {
-            return false;
-        }
-
-        if (
-            ($from != 'INF' && $to == 'INF' && $value < $from) ||
-            ($from == 'INF' && $to != 'INF' && $value > $to) ||
-            ($from != 'INF' && $to != 'INF' && ($value > $to || $value < $from))
-        ) {
-            $this->addValidationError($key, 'Ta wartość powinna mieścić się w przedziale od ' . $from . ' do ' . $to);
-            return false;
-        }
-
         return true;
     }
 
